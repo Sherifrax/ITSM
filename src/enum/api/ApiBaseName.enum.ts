f757076c669733aa67d17a0e7234d5112349b542
Abdalrahman Sherif <Sherifrax@gmail.com>
2025-05-13 09:46:09 +0400

    some fixes

--- src/enum/api/ApiBaseName.enum.ts
+++ src/enum/api/ApiBaseName.enum.ts
@@ -1,5 +1,5 @@
 export enum ApiBaseName {
-    BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://10.20.20.54:5259/",
+    BASE_URL = "http://10.20.20.54:5259/",
 
     GRAFANA_BASE_URL = "http://localhost:3000/",
   }
